88c91293783bb68fcb1b44e2c77adcc2319754c5
Janielton <janieltoncnn@gmail.com>
2021-05-07 16:05:02 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+
+const Home = ()=> {
+    return (
+      <div>
+        <div className="container">
+          <h1>Pagina Home</h1>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
